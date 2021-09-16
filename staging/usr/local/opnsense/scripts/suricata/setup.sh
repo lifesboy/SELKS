3e5912bf4705a5e795b3271f92a9932a36482286
lifesboy <49264651+lifesboy@users.noreply.github.com>
2021-09-16 14:55:30 +0700

    Fix suricata log permission

--- staging/usr/local/opnsense/scripts/suricata/setup.sh
+++ staging/usr/local/opnsense/scripts/suricata/setup.sh
@@ -4,7 +4,7 @@ SURICATA_DIRS="/var/log/suricata"
 
 for SURICATA_DIR in ${SURICATA_DIRS}; do
 	mkdir -p ${SURICATA_DIR}
-	chown -R root:root ${SURICATA_DIR}
+	chown -R logstash:logstash ${SURICATA_DIR}
 	chmod -R 0700 ${SURICATA_DIR}
 done
 
