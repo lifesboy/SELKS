7adf03291117262973bb1757897b2b7de3465fab
lifesboy <49264651+lifesboy@users.noreply.github.com>
2021-10-05 22:51:44 +0700

    Load training data

--- staging/usr/local/opnsense/scripts/ml/dataprocessor.py
+++ staging/usr/local/opnsense/scripts/ml/dataprocessor.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python3
 
 import ray
+import pyarrow
+from ray.data import Dataset
 import common
+
 
 def preprocess(image: bytes) -> bytes:
     return image
 
-class BatchInferModel:
-    def __init__(self):
-        self.model = ImageNetModel()
-    def __call__(self, batch: pd.DataFrame) -> pd.DataFrame:
-        return self.model(batch)
 
-# Load data from storage.
-ds: Dataset = ray.data.read_binary_files("s3://bucket/image-dir")
+ds: Dataset = ray.data.read_csv([
+    common.TRAIN_DATA_DIR + 'Friday-02-03-2018_TrafficForML_CICFlowMeter.csv',
+    common.TRAIN_DATA_DIR + 'Friday-16-02-2018_TrafficForML_CICFlowMeter.csv',
+    common.TRAIN_DATA_DIR + 'Friday-23-02-2018_TrafficForML_CICFlowMeter.csv',
+    common.TRAIN_DATA_DIR + 'Thuesday-20-02-2018_TrafficForML_CICFlowMeter.csv',
+    common.TRAIN_DATA_DIR + 'Thursday-01-03-2018_TrafficForML_CICFlowMeter.csv',
+    common.TRAIN_DATA_DIR + 'Thursday-15-02-2018_TrafficForML_CICFlowMeter.csv',
+    common.TRAIN_DATA_DIR + 'Thursday-22-02-2018_TrafficForML_CICFlowMeter.csv',
+    common.TRAIN_DATA_DIR + 'Wednesday-14-02-2018_TrafficForML_CICFlowMeter.csv',
+    common.TRAIN_DATA_DIR + 'Wednesday-21-02-2018_TrafficForML_CICFlowMeter.csv',
+    common.TRAIN_DATA_DIR + 'Wednesday-28-02-2018_TrafficForML_CICFlowMeter.csv',
+])
 
 # Preprocess the data.
-ds = ds.map(preprocess)
+#ds = ds.map(preprocess)
 
 # Apply GPU batch inference to the data.
-ds = ds.map_batches(BatchInferModel, compute="actors", batch_size=256, num_gpus=1)
+#ds = ds.map_batches(BatchInferModel, compute="actors", batch_size=256, num_gpus=1)
 
 # Save the output.
 ds.write_json("/tmp/results")
