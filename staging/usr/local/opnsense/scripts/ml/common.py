6eb5c52ddda3387ff9a769c08788aae842e4b7b8
tuan.nguyen <tuan.nguyen@drjoy.vn>
2021-10-08 12:58:24 +0700

    create defined dirs

--- staging/usr/local/opnsense/scripts/ml/common.py
+++ staging/usr/local/opnsense/scripts/ml/common.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+from pathlib import Path
+
 DATA_DIR = '/cic/2018/'
-TMP_DIR = '/drl/tmp/'
 TRAIN_DATA_DIR = DATA_DIR + 'Processed Traffic Data for ML Algorithms/'
+
+TMP_DIR = '/drl/tmp/'
+Path(TMP_DIR).mkdir(parents=True, exist_ok=True)
