af7a26e144ad5f5b594f4d2b842a1942f077a1c5
lifesboy <49264651+lifesboy@users.noreply.github.com>
2021-10-21 18:56:24 +0700

    Add ray cluster configs

--- staging/usr/local/opnsense/scripts/ml/common.py
+++ staging/usr/local/opnsense/scripts/ml/common.py
@@ -2,8 +2,19 @@
 
 from pathlib import Path
 
-DATA_DIR = '/cic/2018/'
+import mlflow
+import ray
+
+RAY_HEAD_NODE_ADDRESS = '127.0.0.1:6379'
+MLFLOW_TRACKING_URI = 'http://127.0.0.1:5000'
+
+DATA_DIR = '/Volumes/Extra/workstore/cic/2018/'
 TRAIN_DATA_DIR = DATA_DIR + 'Processed Traffic Data for ML Algorithms/'
 
-TMP_DIR = '/drl/tmp/'
+TMP_DIR = '/Volumes/Extra/workstore/drl/tmp/'
 Path(TMP_DIR).mkdir(parents=True, exist_ok=True)
+
+
+def init_node():
+    ray.init(address=RAY_HEAD_NODE_ADDRESS)
+    mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
