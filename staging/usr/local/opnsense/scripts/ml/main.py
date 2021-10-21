b7227fea043cbf3dab3e751b4e2945dea9642d6d
tuan.nguyen <tuan.nguyen@drjoy.vn>
2021-10-21 13:03:50 +0700

    Using 1 GPU for main

--- staging/usr/local/opnsense/scripts/ml/main.py
+++ staging/usr/local/opnsense/scripts/ml/main.py
@@ -12,7 +12,7 @@ tune.run(
     stop={"episode_reward_mean": 200},
     config={
         "env": "CartPole-v0",
-        "num_gpus": 0,
+        "num_gpus": 1,
         "num_workers": 1,
         "lr": tune.grid_search([0.01, 0.001, 0.0001]),
     },
