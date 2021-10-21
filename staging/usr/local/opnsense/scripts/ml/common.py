af339d75b94261911fdf081bd723ed7872b694ba
lifesboy <49264651+lifesboy@users.noreply.github.com>
2021-10-21 21:59:07 +0700

    Add ml path

--- staging/usr/local/opnsense/scripts/ml/common.py
+++ staging/usr/local/opnsense/scripts/ml/common.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-
+import sys
 from pathlib import Path
 
 import mlflow
 import ray
+
+PATH_ML = '/usr/local/opnsense/scripts/ml'
 
 # RAY_HEAD_NODE_ADDRESS = '127.0.0.1:6379'
 # MLFLOW_TRACKING_URI = 'http://127.0.0.1:5000'
@@ -18,5 +20,6 @@ Path(TMP_DIR).mkdir(parents=True, exist_ok=True)
 
 
 def init_node():
+    sys.path.insert(0, PATH_ML)
     ray.init(address=RAY_HEAD_NODE_ADDRESS)
     mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
