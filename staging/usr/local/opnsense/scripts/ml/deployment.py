6afeffea72925d1d6d1d483c5d7d216f3957e757
tuan.nguyen <tuan.nguyen@drjoy.vn>
2021-11-23 21:21:15 +0700

    Increase replica node of model

--- staging/usr/local/opnsense/scripts/ml/deployment.py
+++ staging/usr/local/opnsense/scripts/ml/deployment.py
@@ -24,7 +24,7 @@ serve.start(http_options={"host": common.MODEL_SERVE_ADDRESS, "port": common.MOD
 # Let's define two models that just print out the data they received.
 
 
-@serve.deployment(name="ServeAnomalyPPOModel", ray_actor_options={"num_gpus": 0.1})
+@serve.deployment(name="ServeAnomalyPPOModel", num_replicas=3, ray_actor_options={"num_gpus": 0.1})
 class ServeAnomalyPPOModel:
     def __init__(self, checkpoint_path) -> None:
         # self.run, self.client = common.init_experiment("anomaly_deployment")
@@ -51,7 +51,7 @@ class ServeAnomalyPPOModel:
         return {"action": action}
 
 
-@serve.deployment(name="model_two", ray_actor_options={"num_gpus": 0})
+@serve.deployment(name="model_two", num_replicas=2, ray_actor_options={"num_gpus": 0})
 def model_two(data):
     # run2, client2 = common.init_experiment('anomaly_deployment')
     run2, client2 = run, client
@@ -65,7 +65,7 @@ def model_two(data):
 
 # max_concurrent_queries is optional. By default, if you pass in an async
 # function, Ray Serve sets the limit to a high number.
-@serve.deployment(name="ComposedModel", max_concurrent_queries=1000, route_prefix="/anomaly")
+@serve.deployment(name="ComposedModel", num_replicas=2, max_concurrent_queries=1000, route_prefix="/anomaly")
 class ComposedModel:
     def __init__(self):
         # self.run, self.client = common.init_experiment("anomaly_deployment")
