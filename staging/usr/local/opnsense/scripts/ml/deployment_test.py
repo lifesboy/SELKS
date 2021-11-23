695f84ef68d7e3e46ea714d0b88b3a99932cab9f
tuan.nguyen <tuan.nguyen@drjoy.vn>
2021-11-23 13:07:47 +0700

    Add model load testing

---
+++ staging/usr/local/opnsense/scripts/ml/deployment_test.py
@@ -0,0 +1,37 @@
+import gym
+from locust import HttpUser, between, task
+import requests
+import random
+import time
+
+import common
+
+run, client = common.init_experiment('anomaly_deployment_test')
+client.set_tag(run_id=run.info.run_id, key=common.TAG_DEPLOYMENT_STATUS, value="Start")
+
+
+class AnomalyDeploymentModelTest(HttpUser):
+    wait_time = between(5, 15)
+
+    def on_start(self):
+        client.set_tag(run_id=run.info.run_id, key=common.TAG_DEPLOYMENT_STATUS, value="Testing")
+
+    @task
+    def anomaly(self):
+        env = gym.make("CartPole-v0")
+        obs = env.reset()
+        print(f"-> Sending observation {obs}")
+        self.client.get("http://selks.ddns.net:8989/anomaly", json={"observation": obs.tolist()})
+        print(f"<- Received response {resp.json() if resp.ok else resp}")
+
+    @task
+    def about(self):
+        self.client.get("/about/")
+
+# if __name__ == "__main__":
+#     locust
+
+client.set_tag(run_id=run.info.run_id, key=common.TAG_DEPLOYMENT_STATUS, value="Done")
+
+# Run: locust -f deployment_test.py
+# Visit: http://0.0.0.0:8089/
