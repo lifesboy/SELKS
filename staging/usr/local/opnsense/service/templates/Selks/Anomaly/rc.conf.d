4e725a67942bb9a7d06539936cba6b76ee0e0b9b
tuan.nguyen <tuan.nguyen@drjoy.vn>
2021-11-30 12:43:16 +0700

    Add Selks.Anomaly module

--- staging/usr/local/opnsense/service/templates/Selks/Anomaly/rc.conf.d
+++ staging/usr/local/opnsense/service/templates/Selks/Anomaly/rc.conf.d
@@ -1,15 +1,15 @@
 {# Macro import #}
 {% from 'OPNsense/Macros/interface.macro' import physical_interface %}
-{% if not helpers.empty('Selks.Anomaly.general.enabled') %}
+{% if not helpers.empty('Selks.SIDS.general.enabled') %}
 suricata_var_script="/usr/local/opnsense/scripts/suricata/setup.sh"
 suricata_enable="YES"
-{% if Selks.Anomaly.general.ips|default("0") == "1" %}
+{% if Selks.SIDS.general.ips|default("0") == "1" %}
 # IPS mode, switch to netmap
 suricata_netmap="YES"
 {% else %}
-# Anomaly mode, pcap live mode
+# SIDS mode, pcap live mode
 {% set addFlags=[] %}
-{%   for intfName in Selks.Anomaly.general.interfaces.split(',') %}
+{%   for intfName in Selks.SIDS.general.interfaces.split(',') %}
 {#     store additional interfaces to addFlags #}
 {%     do addFlags.append(physical_interface(intfName)) %}
 {%   endfor %}
