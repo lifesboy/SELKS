5edfb7f316ed56928ff6d20a7db15eb4d91236ef
tuan.nguyen <tuan.nguyen@drjoy.vn>
2021-12-11 22:48:49 +0700

    enable parsing testing params

--- staging/usr/local/opnsense/scripts/ml/deployment_test.py
+++ staging/usr/local/opnsense/scripts/ml/deployment_test.py
@@ -1,10 +1,28 @@
 import gym
-from locust import HttpUser, between, task
+import os
+import sys
+from locust import HttpUser, between, task, events
 
 import common
 
+# make sure the helper utilities are importable
+sys.path.append(os.getcwd())
+
 run, client = common.init_experiment('anomaly_deployment_test')
 client.set_tag(run_id=run.info.run_id, key=common.TAG_DEPLOYMENT_TEST_STATUS, value="Start")
+
+
+@events.init_command_line_parser.add_listener
+def _(parser):
+    parser.add_argument("--data-source", type=str, include_in_web_ui=True, default="", help="Data source to test")
+    # Set `include_in_web_ui` to False if you want to hide from the web UI
+    parser.add_argument("--serving-url", type=str, include_in_web_ui=True, default="", help="Serving url to test")
+
+
+@events.test_start.add_listener
+def _(environment, **kw):
+    print("Custom argument supplied: data_source=%s" % environment.parsed_options.data_source)
+    print("Custom argument supplied: serving_url=%s" % environment.parsed_options.serving_url)
 
 
 class AnomalyDeploymentModelTest(HttpUser):
@@ -20,6 +38,7 @@ class AnomalyDeploymentModelTest(HttpUser):
         print(f"-> Sending observation {obs}")
         resp = self.client.get("http://selks.ddns.net:6789/anomaly", json={"observation": obs.tolist()})
         print(f"<- Received response {resp.json() if resp.ok else resp}")
+
 
 # if __name__ == "__main__":
 #     locust
