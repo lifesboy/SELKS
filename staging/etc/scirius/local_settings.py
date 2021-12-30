c83f860b986a0ee4611aa73f5dc7c31f8bbd2ea2
lifesboy <lifesboy@yahoo.com>
2021-12-31 00:09:31 +0700

    migrate scirius to postgres sql

--- staging/etc/scirius/local_settings.py
+++ staging/etc/scirius/local_settings.py
@@ -40,6 +40,8 @@ STATIC_ROOT="/var/lib/scirius/static/"
 
 DATABASES = {
   'default': {
+     #'ENGINE': 'django.db.backends.sqlite3',
+     #'NAME': os.path.join(BASE_DIR, 'db', 'db.sqlite3'),
      'ENGINE': 'django.db.backends.postgresql_psycopg2',
      'NAME': 'postgres',
      'USER':'postgres',
