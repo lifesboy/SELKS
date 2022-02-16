0817b4a5c3acb3821fd7d3fbd386d0d49a72c1bf
lifesboy <lifesboy@yahoo.com>
2022-02-16 22:34:13 +0700

    Add listdatasetmetadata command

---
+++ staging/usr/local/opnsense/scripts/ml/management/commands/listdatasetmetadata.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand, CommandError
+import ujson
+from lib.datasetcache import DatasetCache
+
+
+class Command(BaseCommand):
+    help = 'List dataset metadata'
+
+    def handle(self, *args, **options):
+        rc = DatasetCache()
+        if rc.is_changed():
+            rc.create()
+
+        self.stdout.write(ujson.dumps(rc.list_metadata()))
