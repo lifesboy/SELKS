07f57d3c5bb0d4b47c324f0653c4179103ec6cdb
lifesboy <lifesboy@yahoo.com>
2022-02-18 06:10:27 +0700

    Fix unrecognized migration method

--- staging/usr/local/opnsense/scripts/ml/models/metadata_histogram.py
+++ staging/usr/local/opnsense/scripts/ml/models/metadata_histogram.py
@@ -12,7 +12,16 @@ class MetadataHistogram(DbView):
     number_of_datasets = models.IntegerField(blank=True, null=True, db_column='number_of_datasets')
 
     @classmethod
-    def get_view_str(cls):
+    def get_view_str1(cls):
+        return """
+            create view metadata_histogram as
+                select distinct property, value, count(*) as number_of_datasets
+                from  dataset_properties
+                where property not in ('created_at', 'updated_at')
+                group by property, value
+            """
+
+    def get_view_str(self):
         return """
             create view metadata_histogram as
                 select distinct property, value, count(*) as number_of_datasets
