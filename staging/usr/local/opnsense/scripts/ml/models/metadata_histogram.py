76a4d3f32754a86ce9e2bc6a93155c2efac20a60
lifesboy <lifesboy@yahoo.com>
2022-02-18 06:41:16 +0700

    Fix import

--- staging/usr/local/opnsense/scripts/ml/models/metadata_histogram.py
+++ staging/usr/local/opnsense/scripts/ml/models/metadata_histogram.py
@@ -1,6 +1,6 @@
 import logging
 
-from dbview.models import DbView
+from ml.models.dbview.models import DbView
 from django.db import models
 
 request_logger = logging.getLogger('django.request')
@@ -12,16 +12,7 @@ class MetadataHistogram(DbView):
     number_of_datasets = models.IntegerField(blank=True, null=True, db_column='number_of_datasets')
 
     @classmethod
-    def get_view_str1(cls):
-        return """
-            create view metadata_histogram as
-                select distinct property, value, count(*) as number_of_datasets
-                from  dataset_properties
-                where property not in ('created_at', 'updated_at')
-                group by property, value
-            """
-
-    def get_view_str(self):
+    def get_view_str(cls):
         return """
             create view metadata_histogram as
                 select distinct property, value, count(*) as number_of_datasets
