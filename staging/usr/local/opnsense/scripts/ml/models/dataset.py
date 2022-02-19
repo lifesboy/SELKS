da53255d35a651eff166ef516d421df98076fd05
lifesboy <lifesboy@yahoo.com>
2022-02-19 16:57:25 +0700

    Allow null values

--- staging/usr/local/opnsense/scripts/ml/models/dataset.py
+++ staging/usr/local/opnsense/scripts/ml/models/dataset.py
@@ -6,15 +6,15 @@ request_logger = logging.getLogger('django.request')
 
 class Dataset(models.Model):
     sid = models.CharField(primary_key=True, max_length=256)
-    msg = models.CharField(max_length=1000)
-    rev = models.IntegerField(default=0)
-    gid = models.IntegerField(default=0)
-    reference = models.CharField(max_length=10000)
+    msg = models.CharField(max_length=1000, null=True)
+    rev = models.IntegerField(default=0, null=True)
+    gid = models.IntegerField(default=0, null=True)
+    reference = models.CharField(max_length=10000, null=True)
     enabled = models.BooleanField(default=False)
-    action = models.CharField(max_length=10000)
+    action = models.CharField(max_length=10000, null=True)
     source = models.CharField(max_length=10000)
-    created = models.DateField(blank=True, null=True)
-    updated = models.DateField(blank=True, null=True)
+    created_at = models.CharField(max_length=10)
+    updated_at = models.CharField(max_length=10)
 
     def __str__(self):
         return str(self.sid) + ":" + self.action
--- staging/usr/local/opnsense/scripts/ml/models/dataset_properties.py
+++ staging/usr/local/opnsense/scripts/ml/models/dataset_properties.py
@@ -7,7 +7,7 @@ request_logger = logging.getLogger('django.request')
 class DatasetProperties(models.Model):
     sid = models.CharField(primary_key=True, max_length=256)
     property = models.CharField(max_length=10000)
-    value = models.CharField(max_length=10000)
+    value = models.CharField(max_length=10000, null=True)
 
     def __init__(self, *args, **kwargs):
         models.Model.__init__(self, *args, **kwargs)
