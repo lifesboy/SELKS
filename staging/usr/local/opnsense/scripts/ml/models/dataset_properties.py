362b7319681d2468dfef712010c04e08026169e4
lifesboy <lifesboy@yahoo.com>
2022-02-19 17:55:51 +0700

    remove incorrect primary key

--- staging/usr/local/opnsense/scripts/ml/models/dataset_properties.py
+++ staging/usr/local/opnsense/scripts/ml/models/dataset_properties.py
@@ -5,8 +5,8 @@ request_logger = logging.getLogger('django.request')
 
 
 class DatasetProperties(models.Model):
-    sid = models.CharField(primary_key=True, max_length=256)
-    property = models.CharField(primary_key=True, max_length=10000)
+    sid = models.CharField(max_length=256)
+    property = models.CharField(max_length=10000)
     value = models.CharField(max_length=10000, null=True)
 
     def __init__(self, *args, **kwargs):
