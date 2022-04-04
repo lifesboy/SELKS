56ae5bffa53e9d0d048cfe5d1700d1316e341e74
lifesboy <lifesboy@yahoo.com>
2022-04-04 21:00:11 +0700

    Fix c-icap log permission for collecting by elk

--- staging/usr/local/opnsense/scripts/OPNsense/CICAP/setup.sh
+++ staging/usr/local/opnsense/scripts/OPNsense/CICAP/setup.sh
@@ -6,7 +6,7 @@ chmod 750 /var/run/c-icap
 
 mkdir -p /var/log/c-icap
 chown -R root:root /var/log/c-icap
-chmod 754 /var/log/c-icap
+chmod 755 /var/log/c-icap
 (cd /var/log && ln -s c-icap cicap)
 chown -R root:root /var/log/cicap
 
