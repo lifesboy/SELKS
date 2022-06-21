1c17ae26e7878b8314c0f3f764b812e635bbb982
tuan.nguyen <tuan.nguyen@drjoy.vn>
2022-06-21 13:01:01 +0700

    Migate utils

---
+++ staging/usr/local/opnsense/scripts/ml/lib/logger.py
@@ -0,0 +1,39 @@
+import logging
+
+LOG_INFO_FILE = '/var/log/cicflowmeter/cicflowmeter.log'
+LOG_ERROR_FILE = '/var/log/cicflowmeter/error.log'
+
+_log_format = f"%(asctime)s[%(levelname)s][%(thread)d-%(threadName)s] %(filename)s:%(funcName)s(%(lineno)d) %(message)s"
+
+
+def get_file_handler():
+    file_handler = logging.FileHandler(LOG_INFO_FILE)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(logging.Formatter(_log_format))
+    return file_handler
+
+
+def get_file_handler_error():
+    file_handler = logging.FileHandler(LOG_ERROR_FILE)
+    file_handler.setLevel(logging.ERROR)
+    file_handler.setFormatter(logging.Formatter(_log_format))
+    return file_handler
+
+
+def get_stream_handler():
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    stream_handler.setFormatter(logging.Formatter(_log_format))
+    return stream_handler
+
+
+def get_logger(name: str = None, level: int = logging.DEBUG):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(get_file_handler())
+    logger.addHandler(get_file_handler_error())
+    logger.addHandler(get_stream_handler())
+    return logger
+
+
+log = get_logger()
---
+++ staging/usr/local/opnsense/scripts/ml/lib/utils.py
@@ -0,0 +1,61 @@
+import uuid
+from itertools import islice, zip_longest
+from datetime import datetime
+from .util.logger import log
+
+import hashlib
+import numpy
+
+
+def marked_done(files: []) -> bool:
+    t = str(datetime.now())
+    fs = list(map(lambda i: open(i, 'w'), files))
+    list(map(lambda i: i.write(t), fs))
+    list(map(lambda i: i.close(), fs))
+    return True
+
+
+def get_output_file_of_batch(names: []) -> str:
+    return '%s_%s.csv' % ('_'.join(names), datetime.now().strftime("%Y-%m-%dT%H:%M:%S"))
+
+
+def get_marked_done_file_name(file: str) -> str:
+    return '.%s.done.cic' % hashlib.sha256(file.encode('utf-8')).hexdigest()
+
+
+def grouper(iterable, n, max_groups=0, fillvalue=None):
+    """Collect data into fixed-length chunks or blocks"""
+
+    if max_groups > 0:
+        iterable = islice(iterable, max_groups * n)
+
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
+
+
+def random_string():
+    return uuid.uuid4().hex[:6].upper().replace("0", "X").replace("O", "Y")
+
+
+def get_statistics(alist: list):
+    """Get summary statistics of a list"""
+    iat = dict()
+    try:
+        if len(alist) > 1:
+            iat["total"] = sum(alist)
+            iat["max"] = max(alist)
+            iat["min"] = min(alist)
+            alist_float = list(map(float, alist))
+            iat["mean"] = numpy.mean(alist_float)
+            iat["std"] = numpy.sqrt(numpy.var(alist_float))
+        else:
+            iat["total"] = 0
+            iat["max"] = 0
+            iat["min"] = 0
+            iat["mean"] = 0
+            iat["std"] = 0
+    except Exception as e:
+        log.error('statistics %s error: %s', alist, e)
+        raise e
+
+    return iat
