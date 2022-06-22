b03797000b276e292c005daeeef8f7f96d67cf7e
tuan.nguyen <tuan.nguyen@drjoy.vn>
2022-06-22 13:04:53 +0700

    add lib

--- staging/usr/local/opnsense/scripts/ml/detection.py
+++ staging/usr/local/opnsense/scripts/ml/detection.py
@@ -1,5 +1,7 @@
 import argparse
 import glob
+import os
+
 import pandas as pd
 from pandas import DataFrame
 
@@ -13,14 +15,14 @@ import ray.rllib.agents.ppo as ppo
 from ray import serve
 
 import common
+from lib import utils
 
-for _ in range(1000):
+def detect(packet):
     env = gym.make("CartPole-v0")
     obs = env.reset()
     print(f"-> Sending observation {obs}")
     resp = requests.get(common.MODEL_SERVE_DETECTION_URL, json={"observation": obs.tolist()})
     print(f"<- Received response {resp.json() if resp.ok else resp}")
-    time.sleep(random.randint(1, 5))
 
 
 def main():
