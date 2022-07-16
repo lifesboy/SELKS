8bd3d67028d742e190d262b7ea977e7594756691
lifesboy <lifesboy@yahoo.com>
2022-07-16 14:25:22 +0700

    Fix empty data frame

--- staging/usr/local/opnsense/scripts/ml/lib/utils.py
+++ staging/usr/local/opnsense/scripts/ml/lib/utils.py
@@ -26,20 +26,20 @@ def get_marked_done_file_name(file: str, tag: str = '_') -> str:
 def get_processing_file_pattern(
         input_files: [], output: str, ext: str = '', tag: str = '_', batch_size: int = 10) -> DataFrame:
     file_df: DataFrame = pd.DataFrame(input_files, columns=['input_path'])
-    file_df['st_mtime'] = file_df.apply(lambda i: os.stat(i.input_path).st_mtime, axis=1)
-    file_df['st_size'] = file_df.apply(lambda i: os.stat(i.input_path).st_size, axis=1)
+    file_df['st_mtime'] = file_df.apply(lambda i: os.stat(i.input_path).st_mtime, axis=1, result_type='reduce')
+    file_df['st_size'] = file_df.apply(lambda i: os.stat(i.input_path).st_size, axis=1, result_type='reduce')
     file_df = file_df[file_df['st_size'] > 0]
-    file_df['input_name'] = file_df.apply(lambda i: os.path.split(i.input_path)[-1], axis=1)
-    file_df['marked_done_name'] = file_df.apply(lambda i: get_marked_done_file_name(i.input_path, tag), axis=1)
-    file_df['marked_done_path'] = file_df.apply(lambda i: os.path.join(output, i.marked_done_name), axis=1)
-    file_df['marked_done_existed'] = file_df.apply(lambda i: os.path.exists(i.marked_done_path), axis=1)
+    file_df['input_name'] = file_df.apply(lambda i: os.path.split(i.input_path)[-1], axis=1, result_type='reduce')
+    file_df['marked_done_name'] = file_df.apply(lambda i: get_marked_done_file_name(i.input_path, tag), axis=1, result_type='reduce')
+    file_df['marked_done_path'] = file_df.apply(lambda i: os.path.join(output, i.marked_done_name), axis=1, result_type='reduce')
+    file_df['marked_done_existed'] = file_df.apply(lambda i: os.path.exists(i.marked_done_path), axis=1, result_type='reduce')
 
     file_df = file_df.loc[file_df['marked_done_existed'] == False]
     file_df = file_df.sort_values(by='input_name')
     file_df = file_df.filter(['input_path', 'input_name', 'marked_done_path', 'st_mtime']).applymap(lambda i: [i])
-    file_df['batch'] = file_df.apply(lambda i: i.name // batch_size, axis=1)
+    file_df['batch'] = file_df.apply(lambda i: i.name // batch_size, axis=1, result_type='reduce')
 
     batch_df: DataFrame = file_df.groupby('batch').sum()
-    batch_df['output_name'] = batch_df.apply(lambda i: get_output_file_of_batch(i.input_name, tag, ext), axis=1)
-    batch_df['output_path'] = batch_df.apply(lambda i: os.path.join(output, i.output_name), axis=1)
+    batch_df['output_name'] = batch_df.apply(lambda i: get_output_file_of_batch(i.input_name, tag, ext), axis=1, result_type='reduce')
+    batch_df['output_path'] = batch_df.apply(lambda i: os.path.join(output, i.output_name), axis=1, result_type='reduce')
     return batch_df
