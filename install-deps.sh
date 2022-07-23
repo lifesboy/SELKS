a446902dc5ed0a24f9087a316c5694e69b4f7d40
lifesboy <lifesboy@yahoo.com>
2022-07-23 21:30:05 +0700

    update docutils

--- install-deps.sh
+++ install-deps.sh
@@ -8,7 +8,7 @@
 set -e
 
 # Install needed packages for default build
-apt-get -y install xorriso live-build syslinux squashfs-tools python-docutils
+apt-get -y install xorriso live-build syslinux squashfs-tools python3-docutils
 
 # Install needed packages for the choose your own kernel option
 apt-get -y install wget fakeroot gcc libncurses5-dev bc \
