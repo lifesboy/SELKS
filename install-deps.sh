99e235661f87eabbfa456c8e5a458745e9eb148a
lifesboy <lifesboy@yahoo.com>
2022-07-24 09:32:14 +0700

    Required bc to build 5x kernel

--- install-deps.sh
+++ install-deps.sh
@@ -8,7 +8,7 @@
 set -e
 
 # Install needed packages for default build
-apt-get -y install xorriso live-build syslinux squashfs-tools python3-docutils
+apt-get -y install xorriso live-build syslinux squashfs-tools python3-docutils bc
 
 # Install needed packages for the choose your own kernel option
 apt-get -y install wget fakeroot gcc libncurses5-dev bc \
