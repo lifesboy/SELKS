475578c3bc8016c432677f19f1fe46957dcc9bea
lifesboy <lifesboy@yahoo.com>
2022-09-01 21:32:34 +0700

    Add lint type

--- staging/usr/local/opnsense/scripts/ml/aienvs/anomaly/anomaly_env.py
+++ staging/usr/local/opnsense/scripts/ml/aienvs/anomaly/anomaly_env.py
@@ -6,29 +6,31 @@ import numpy as np
 import common
 
 
+from typing import Iterator
 # @ray.remote
-from ray.data import Dataset
+from ray.data.dataset import Dataset, BatchType
 from anomaly_normalization import DST_PORT, PROTOCOL, FLOW_DURATION, TOT_FWD_PKTS, TOT_BWD_PKTS, LABEL
 
 
 class AnomalyEnv(gym.Env):
     """Env in which the observation at timestep minus n must be repeated."""
 
-    def __init__(self, config=None):
+    def __init__(self, config: dict = None):
         self._run, self._client = common.init_experiment(name='anomaly-model', run_name='env-tuning-%s' % time.time())
         self._client.set_tag(run_id=self._run.info.run_id, key=common.TAG_RUN_TAG, value='env-tuning')
+
         config = config or {}
-        self.data_source_sampling_dir = config.get("data_source_sampling_dir", [])
+        self.data_source_sampling_dir: str = config.get("data_source_sampling_dir", [])
         self.data_set: Dataset = ray.data.read_csv(self.data_source_sampling_dir)
 
-        self.iter = self.data_set.window(blocks_per_window=1024).iter_batches(batch_size=1)
+        self.iter: Iterator[BatchType] = self.data_set.window(blocks_per_window=1024).iter_batches(batch_size=1)
 
-        self.observation_space = Box(low=0., high=1., shape=(6,), dtype=np.float64)
-        self.action_space = Discrete(2)
+        self.observation_space: Box = Box(low=0., high=1., shape=(6,), dtype=np.float64)
+        self.action_space: Discrete = Discrete(2)
 
-        self.episode_len = config.get("episode_len", 100)
+        self.episode_len: int = config.get("episode_len", 100)
         self.current_obs = None
-        self.current_len = 0
+        self.current_len: int = 0
 
     def reset(self):
         self.current_obs = None
@@ -36,7 +38,7 @@ class AnomalyEnv(gym.Env):
         self.iter = self.data_set.window(blocks_per_window=1024).iter_batches(batch_size=1)
         return self._next_obs()
 
-    def step(self, action):
+    def step(self, action: np.float64):
         if (self.current_obs is None) or (action == self.current_obs[-1]):
             reward = 1
         else:
