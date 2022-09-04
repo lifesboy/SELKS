5b78a2fbe3bb526cc4332f36af86574f46ab0f77
lifesboy <lifesboy@yahoo.com>
2022-09-04 23:14:50 +0700

    skip error on calculate model signature

--- staging/usr/local/opnsense/scripts/ml/aimodels/anomaly/anomaly_model.py
+++ staging/usr/local/opnsense/scripts/ml/aimodels/anomaly/anomaly_model.py
@@ -106,9 +106,9 @@ class AnomalyModel(RecurrentNetwork):
         # self.rnn_model.export_saved_model("/tmp/anomaly_model/", receiver_fn).decode("utf-8")
         # tf.keras.experimental.export_saved_model(self.rnn_model, "/tmp/anomaly_model/")
         model_meta = AnomalyModel.get_model_meta()
-        signature = infer_signature(inputs, self._value_out)
+        # signature = infer_signature(inputs, self._value_out)
         mlflow.keras.log_model(keras_model=self.rnn_model,
-                               signature=signature,
+                               # signature=signature,
                                artifact_path=model_meta.artifact_path,
                                registered_model_name=model_meta.registered_model_name)
 
