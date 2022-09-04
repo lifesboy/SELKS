c2219a3ac77e7bf6ac19bb18aeca6e71c68f59fb
lifesboy <lifesboy@yahoo.com>
2022-09-05 00:04:32 +0700

    update infer data format

--- staging/usr/local/opnsense/scripts/ml/aideployments/anomaly/anomaly_production_deployment.py
+++ staging/usr/local/opnsense/scripts/ml/aideployments/anomaly/anomaly_production_deployment.py
@@ -35,8 +35,6 @@ class AnomalyProductionDeployment:
     async def predict(self, df):
         return self.model.predict(df).to_json(orient="records")
 
-    async def _process_request_data(self, request: Request) -> pd.DataFrame:
-        body = await request.body()
-        if isinstance(body, pd.DataFrame):
-            return body
-        return pd.read_json(json.loads(body))
+    async def _process_request_data(self, request: Request):
+        data = await request.body()
+        return data['obs']
