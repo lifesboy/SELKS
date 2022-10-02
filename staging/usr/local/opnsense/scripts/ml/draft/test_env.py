550954e73ed272c593e035079b32339198b71901
lifesboy <lifesboy@yahoo.com>
2022-10-03 00:30:19 +0700

    test feature order

--- staging/usr/local/opnsense/scripts/ml/draft/test_env.py
+++ staging/usr/local/opnsense/scripts/ml/draft/test_env.py
@@ -1,12 +1,15 @@
 import ray
 import numpy as np
 
-a = ray.data.from_items([{"A": x % 3, "B": x, "C": 0.5 * x} for x in range(1)])
+a = ray.data.from_items([{"A": x % 3, "B": 1 + x, "C": 0.5 * x} for x in range(1)])
 df = a.to_pandas()
 print(df)
 
-df = df[["A", "B"]]
+df = df[["B", "A"]]
 print(df)
 
 b = df.to_numpy(dtype=np.float64).flatten()
 print(b)
+
+token = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6], np.float64)
+print(token)
