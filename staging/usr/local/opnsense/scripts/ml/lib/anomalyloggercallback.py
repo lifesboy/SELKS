daf004e5d745adb48f49d75411b2c335e131744b
lifesboy <lifesboy@yahoo.com>
2022-10-13 22:56:53 +0700

    temp disable log param due to limit 500 length error

--- staging/usr/local/opnsense/scripts/ml/lib/anomalyloggercallback.py
+++ staging/usr/local/opnsense/scripts/ml/lib/anomalyloggercallback.py
@@ -5,6 +5,22 @@ from ray.tune.integration.mlflow import MLflowLoggerCallback
 
 class AnomalyLoggerCallback(MLflowLoggerCallback):
 
+    def log_trial_start(self, trial: "Trial"):
+        # Create run if not already exists.
+        if trial not in self._trial_runs:
+            # Set trial name in tags
+            tags = self.tags.copy()
+            tags["trial_name"] = str(trial)
+
+            run = self.mlflow_util.start_run(tags=tags, run_name=str(trial))
+            self._trial_runs[trial] = run.info.run_id
+
+        run_id = self._trial_runs[trial]
+
+        # Log the config parameters.
+        config = trial.config
+        # self.mlflow_util.log_params(run_id=run_id, params_to_log=config)
+
     def log_trial_result(self, iteration: int, trial: "Trial", result: Dict):
         super().log_trial_result(iteration=iteration, trial=trial, result=result)
         run_id = self._trial_runs[trial]
