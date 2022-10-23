bd7332f5064cc74f0ff95734c74bd6656c08e351
lifesboy <lifesboy@yahoo.com>
2022-10-23 09:11:49 +0700

    clean state table when shutting down vpn

--- staging/usr/local/etc/inc/plugins.inc.d/openvpn/attributes.sh
+++ staging/usr/local/etc/inc/plugins.inc.d/openvpn/attributes.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 if [ "$script_type" = "client-disconnect" ]; then
-	/sbin/pfctl -k $ifconfig_pool_remote_ip
-	/sbin/pfctl -K $ifconfig_pool_remote_ip
+	# /sbin/pfctl -k $ifconfig_pool_remote_ip
+	# /sbin/pfctl -K $ifconfig_pool_remote_ip
+	# TODO kill state of $ifconfig_pool_remote_ip only
+	/usr/sbin/conntrack -F
 fi
 
 exit 0
