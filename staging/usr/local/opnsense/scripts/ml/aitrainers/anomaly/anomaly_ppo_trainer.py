2e4eb219d34cb096490ed062460a8445a91a11df
lifesboy <lifesboy@yahoo.com>
2022-12-21 20:02:58 +0700

    fix depreciated tf save model

--- staging/usr/local/opnsense/scripts/ml/aitrainers/anomaly/anomaly_ppo_trainer.py
+++ staging/usr/local/opnsense/scripts/ml/aitrainers/anomaly/anomaly_ppo_trainer.py
@@ -15,18 +15,20 @@ class AnomalyPPOTrainer(PPOTrainer):
         path = super().save_checkpoint(checkpoint_dir)
         self.export_policy_model(os.path.join(checkpoint_dir, "policy"))
 
-        h5_path = os.path.join(checkpoint_dir, "h5", "saved_model.h5")
-        # self.get_policy().model.base_model.save_weights(h5_path)
-        # self.get_policy().model.base_model.save(h5_path)
+        with self.get_policy()._sess.graph.as_default():
+            with self.get_policy()._sess.as_default():
+                h5_path = os.path.join(checkpoint_dir, "h5", "saved_model.h5")
+                self.get_policy().model.base_model.save_weights(h5_path)
+                self.get_policy().model.base_model.save(h5_path)
 
-        model_meta = AnomalyModel.get_model_meta()
-        features: [str] = self.config.get('features', [])
-        input_schema = Schema([ColSpec(type=DataType.double, name=i) for i in features])
-        output_schema = Schema([ColSpec(type=DataType.integer, name=LABEL)])
-        signature = ModelSignature(inputs=input_schema, outputs=output_schema)
-        mlflow.keras.log_model(keras_model=self.get_policy().model.base_model,
-                               signature=signature,
-                               artifact_path=model_meta.artifact_path,
-                               registered_model_name=model_meta.registered_model_name)
+                model_meta = AnomalyModel.get_model_meta()
+                features: [str] = self.config.get('features', [])
+                input_schema = Schema([ColSpec(type=DataType.double, name=i) for i in features])
+                output_schema = Schema([ColSpec(type=DataType.integer, name=LABEL)])
+                signature = ModelSignature(inputs=input_schema, outputs=output_schema)
+                mlflow.keras.log_model(keras_model=self.get_policy().model.base_model,
+                                       signature=signature,
+                                       artifact_path=model_meta.artifact_path,
+                                       registered_model_name=model_meta.registered_model_name)
 
         return path
