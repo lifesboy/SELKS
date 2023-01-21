ad000d5d4994b4ec598d5cbda090d243c5c5855e
lifesboy <lifesboy@yahoo.com>
2023-01-21 21:59:23 +0700

    fix non ref on remote calls

--- staging/usr/local/opnsense/scripts/ml/aimodels/preprocessing/cicflowmeter_norm_model.py
+++ staging/usr/local/opnsense/scripts/ml/aimodels/preprocessing/cicflowmeter_norm_model.py
@@ -317,12 +317,12 @@ class CicFlowmeterNormModel(mlflow.pyfunc.PythonModel):
         #     df_norm[LABEL] = ''
 
         @ray.remote
-        def transform(ds, f):
-            return list(tf.data.Dataset.from_tensor_slices(tf.convert_to_tensor(ds))
-                        .map(f)
-                        .as_numpy_iterator())
+        def transform(name, ds, f):
+            return (name, list(tf.data.Dataset.from_tensor_slices(tf.convert_to_tensor(ds))
+                               .map(f)
+                               .as_numpy_iterator()))
 
-        pipes = map(lambda x: (x, transform.remote(df_norm[x], feature_norm[x])), features)
+        pipes = map(lambda x: transform.remote(df_norm[x], feature_norm[x]), features)
         transformed = ray.get(list(pipes))
         data = DataFrame(data=dict(transformed))
 
