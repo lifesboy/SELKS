9adbb80f3799376cdb8290b424426046f023de91
lifesboy <lifesboy@yahoo.com>
2023-01-27 16:12:49 +0700

    shorten normalized file name

--- staging/usr/local/opnsense/scripts/ml/lib/singlefileblockwritepathprovider.py
+++ staging/usr/local/opnsense/scripts/ml/lib/singlefileblockwritepathprovider.py
@@ -21,7 +21,7 @@ class SingleFileBlockWritePathProvider(DefaultBlockWritePathProvider):
             block_index: Optional[int] = None,
             file_format: Optional[str] = None,
     ) -> str:
-        suffix = f"{self.file_name}.{dataset_uuid}_{block_index:06}.{file_format}"
+        suffix = f"{self.file_name}.{dataset_uuid[:6]}_{block_index:06}.{file_format}"
         # Uses POSIX path for cross-filesystem compatibility, since PyArrow
         # FileSystem paths are always forward slash separated, see:
         # https://arrow.apache.org/docs/python/filesystems.html
