4df299b453f6752aac4c206b26d92e6dd22cbaa0
lifesboy <lifesboy@yahoo.com>
2023-02-05 09:08:48 +0700

    keep 5 checkpoint only to fix full disk

--- staging/usr/local/opnsense/scripts/ml/trainanomaly.py
+++ staging/usr/local/opnsense/scripts/ml/trainanomaly.py
@@ -303,7 +303,7 @@ def main(args, course: str, unit: str, lesson: str, lab: str):
                         trial_name_creator=lambda _: lesson,
                         trial_dirname_creator=lambda _: lesson,
                         # log_to_file=['stdout.txt', 'stderr.txt'],  #not use this, ray error I/O on closed stream
-                        keep_checkpoints_num=20,
+                        keep_checkpoints_num=5,
                         checkpoint_freq=1,
                         checkpoint_at_end=True,
                         export_formats=[ExportFormat.MODEL, ExportFormat.H5, ExportFormat.CHECKPOINT],
