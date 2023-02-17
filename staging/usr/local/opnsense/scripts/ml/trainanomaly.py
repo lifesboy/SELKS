286a900ad07156bd15123e356a777d68d80da31f
lifesboy <lifesboy@yahoo.com>
2023-02-17 07:24:41 +0700

    skip checkpoint by accuracy

--- staging/usr/local/opnsense/scripts/ml/trainanomaly.py
+++ staging/usr/local/opnsense/scripts/ml/trainanomaly.py
@@ -315,6 +315,7 @@ def main(args, course: str, unit: str, lesson: str, lab: str):
                         keep_checkpoints_num=5,
                         checkpoint_freq=1,
                         checkpoint_at_end=True,
+                        checkpoint_score_attr="accuracy",
                         export_formats=[ExportFormat.MODEL, ExportFormat.H5, ExportFormat.CHECKPOINT],
                         resume=resume,
                         callbacks=[AnomalyLoggerCallback(
