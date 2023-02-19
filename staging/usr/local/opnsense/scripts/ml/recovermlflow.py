1b2b3beba72428101123216b95aa7940dd209478
lifesboy <lifesboy@yahoo.com>
2023-02-19 17:40:41 +0700

    passing mlflow client

--- staging/usr/local/opnsense/scripts/ml/recovermlflow.py
+++ staging/usr/local/opnsense/scripts/ml/recovermlflow.py
@@ -131,7 +131,7 @@ if __name__ == "__main__":
         older_than = 30
         client.log_param(run_id=run.info.run_id, key='auto-clean.older_than', value=f"{older_than}d")
 
-        clean_result = utils.clean_mlflow(older_than)
+        clean_result = utils.clean_mlflow(client, older_than)
         client.log_text(run_id=run.info.run_id, text=clean_result, artifact_file='clean_result.txt')
 
         main(args)
