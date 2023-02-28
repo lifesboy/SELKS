d1bba00ccd7748e3160584b5cbfb9568e0cbe4da
lifesboy <lifesboy@yahoo.com>
2023-02-28 19:50:01 +0700

    Revert "fix duplicated metric error"
    This reverts commit 0594d6857667886002ebae785c2327f3f579365e.

--- staging/usr/local/opnsense/scripts/ml/lib/mlflow_helper.py
+++ staging/usr/local/opnsense/scripts/ml/lib/mlflow_helper.py
@@ -5,4 +5,4 @@ from mlflow.tracking.client import MlflowClient
 def log_batch_heavy(client: MlflowClient, metrics: [SqlLatestMetric]):
     store = client._tracking_client.store
     with store.ManagedSessionMaker() as session:
-        session.bulk_save_objects(metrics)
+        session.add_all(metrics)
