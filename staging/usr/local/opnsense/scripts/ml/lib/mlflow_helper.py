70f9d4f0cb31ea176c5ba00eefff43d1c2b50145
lifesboy <lifesboy@yahoo.com>
2023-02-28 20:15:19 +0700

    Revert "saving by db client"
    This reverts commit faa27c4aefae3651f9c6562073e05fcd53f06df7.

--- staging/usr/local/opnsense/scripts/ml/lib/mlflow_helper.py
+++ staging/usr/local/opnsense/scripts/ml/lib/mlflow_helper.py
@@ -1,11 +1,8 @@
 from mlflow.store.tracking.dbmodels.models import SqlLatestMetric
 from mlflow.tracking.client import MlflowClient
 
-DATABASE_URI = 'postgresql://postgres:postgres@127.0.0.1:5432/postgres'
 
-
-def log_batch_heavy(metrics: [SqlLatestMetric]):
-    client = MlflowClient(tracking_uri=DATABASE_URI)
+def log_batch_heavy(client: MlflowClient, metrics: [SqlLatestMetric]):
     store = client._tracking_client.store
     with store.ManagedSessionMaker() as session:
         session.bulk_save_objects(metrics)
