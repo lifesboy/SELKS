0594d6857667886002ebae785c2327f3f579365e
lifesboy <lifesboy@yahoo.com>
2023-02-28 19:43:56 +0700

    fix duplicated metric error

--- staging/usr/local/opnsense/scripts/ml/lib/mlflow_helper.py
+++ staging/usr/local/opnsense/scripts/ml/lib/mlflow_helper.py
@@ -5,4 +5,4 @@ from mlflow.tracking.client import MlflowClient
 def log_batch_heavy(client: MlflowClient, metrics: [SqlLatestMetric]):
     store = client._tracking_client.store
     with store.ManagedSessionMaker() as session:
-        session.add_all(metrics)
+        session.bulk_save_objects(metrics)
