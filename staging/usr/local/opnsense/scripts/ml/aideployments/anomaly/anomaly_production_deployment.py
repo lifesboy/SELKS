e62272a34de1cb6e06a1888ef75c366111345c46
lifesboy <lifesboy@yahoo.com>
2023-03-11 07:38:39 +0700

    fix (psycopg2.ProgrammingError) can't adapt type 'numpy.int64'

--- staging/usr/local/opnsense/scripts/ml/aideployments/anomaly/anomaly_production_deployment.py
+++ staging/usr/local/opnsense/scripts/ml/aideployments/anomaly/anomaly_production_deployment.py
@@ -92,7 +92,7 @@ class AnomalyProductionDeployment:
             obs_labeled = await self.predict(obs, batch_size, anomaly_threshold)
             res = await self._process_response_data(obs_labeled)
             self.batches_success += 1
-            self.anomaly_detected += obs_labeled[LABEL].sum()
+            self.anomaly_detected += int(obs_labeled[LABEL].sum())
             self.metrics += [
                 Metric(key='anomaly_detected', value=self.anomaly_detected, timestamp=timestamp, step=self.current_step),
                 Metric(key='batches_success', value=self.batches_success, timestamp=timestamp, step=self.current_step),
